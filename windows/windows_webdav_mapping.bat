766cdb17cef20bac3766338e56fc4326f8a78570
gabrielvon <fwlg1113@gmail.com>
2021-11-02 14:52:35 +0800

    feat: add `RightClickVSCode.reg` and move windows relative files into windows folder

---
+++ windows/windows_webdav_mapping.bat
@@ -0,0 +1,9 @@
+@REM @Author: Gabriel
+@REM @Date:   2021-10-26 08:08:44
+@REM @Last Modified by:   Gabriel
+@REM Modified time: 2021-10-26 08:09:28
+
+
+
+
+net use Y: \\JEOUU\UPGREAT "RDpassword" /u:"ADMINISTRATOR"
