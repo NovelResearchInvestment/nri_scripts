5abeab825ea0d734b42d92ce7cf27f0f5ecf1dfe
Gabriel Feng <gabrielwfeng@gmail.com>
2021-12-16 13:23:07 +0800

    feat: simplify

--- centos/centos_setup.sh
+++ centos/centos_setup.sh
@@ -2,7 +2,7 @@
 ###
  # @Author: Gabriel Feng
  # @Date: 2021-10-28 20:15:13
- # @LastEditTime: 2021-12-16 12:24:13
+ # @LastEditTime: 2021-12-16 13:13:50
  # @LastEditors: Gabriel Feng
  # @Description:
  # @FilePath: \NRI\apps\nri_scripts\centos\centos_setup.sh
@@ -21,12 +21,11 @@ yum install -y vim curl git wget tree zsh tmux
 export TZ=Asia/Shanghai
 timedatectl set-timezone $(timedatectl list-timezones | grep -i Asia/Shanghai)
 
-# install vim
-mkdir ~/.vim_runtime
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+# # install awesome vim
+# git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+# sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-# git clone https://github.com/tell-k/vim-autopep8.git ~/.vim_runtime/my_plugins/vim-rails
+# git clone --depth=1 https://github.com/tell-k/vim-autopep8.git ~/.vim_runtime/my_plugins/vim-rails
 # python3 -m pip install pynvim
 # echo "let g:python3_host_prog='/usr/bin/python3'" >> ~/.vimrc
 # echo "Plugin 'Chiel92/vim-autoformat'" >> ~/.vimrc
@@ -34,20 +33,20 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 #install tmux
 printf 'yyy' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  &&
 cd ~
-git clone https://github.com/gpakosz/.tmux.git
+git clone --depth=1 https://github.com/gpakosz/.tmux.git
 ln -s -f ~/.tmux/.tmux.conf
 cp ~/.tmux/.tmux.conf.local .tmux.conf.local
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 curl https://raw.githubusercontent.com/NovelResearchInvestment/nri_scripts/dev/centos/tmux.conf > ~/.tmux.conf
 
 # zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # make directories
 mkdir ~/Downloads
 mkdir ~/Repos
 
-yum clean all
+yum clean all && rm -rf /var/cache/yum
